f51060c503b08a34d64e284017a079380796121f
Sina-Amare <sinaamareh0263@gmail.com>
2023-04-14 17:07:53 +0330

    making migrations

--- First_Api/profiles/models.py
+++ First_Api/profiles/models.py
@@ -1,8 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
-from user_profile.models import User
+from django.contrib.auth.models import BaseUserManager
 
+class UserProfileManager(BaseUserManager):
+    
+    def create_user(self, email , name , password = None):
+        
+        if not email :
+            raise ValueError("User Must Have an Email Address")
+        
+        email = self.normalize_email(email)
+        user = self.model(email = email, name = name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
+    
+    def create_superuser(self, email , name , password):
+        
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        
+        return user       
+    
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """ DataBase Model for UserProfile """
     email = models.EmailField(max_length=254 , unique = True)
@@ -17,4 +42,12 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     def get_full_name(self):
         """ Get the full name of user"""
         return self.name
+    
+    def get_short_name(self):
+        return self.name
+    
+    def __str__(self):
+        return self.email
+    
+    
     
