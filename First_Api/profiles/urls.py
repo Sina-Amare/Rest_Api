1cead88de3d9f43476b18b82bee2ef797164d965
Sina-Amare <sinaamareh0263@gmail.com>
2023-05-28 15:32:33 +0330

    added put-patch-delete

--- First_Api/profiles/urls.py
+++ First_Api/profiles/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from profiles import views
 
 urlpatterns = [
-    path("hello-view", views.HelloApiView.as_view())
+    path("hello-view/", views.HelloApiView.as_view())
 ]
--- First_Api/profiles/views.py
+++ First_Api/profiles/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.views import Response
 from rest_framework.views import status
 from profiles import serializers
+import requests
 class HelloApiView(APIView):
     
     serializer_class = serializers.HelloSerializer
@@ -21,6 +22,7 @@ class HelloApiView(APIView):
         })
         
     def post(self, request):
+        #Create a new Object
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             name = serializer.validated_data.get('name')
@@ -29,3 +31,15 @@ class HelloApiView(APIView):
         else:
             return Response(serializer.errors,
                             status= status.HTTP_400_BAD_REQUEST)
+    def put(self, request , pk = None):
+        name = request.data.get('name')
+        return Response({'method': 'PUT'})
+    #update the whole object
+    
+    def patch(self,request , pk = None):
+        return Response({'method': 'PATCH'})
+    #Update a partial object
+    
+    def delete(self,request, pk = None):
+        return Response({'method': 'DELETE'})
+    #Delete
